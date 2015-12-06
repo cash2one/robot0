3b49aefd0091b155d495c73d14ab0ff1b7947b22
nickgu <pi@raspberrypi.(none)>
2015-12-06 10:31:29 +0800

    change code, add step-moter class

---
+++ step-motor/stepmoter.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# gusimiu@baidu.com
+# 
+
+import time
+import RPi.GPIO as GPIO
+import sys
+
+class StepMoter:
+    Clockwise, InterClockwise = range(2)
+    Fast, Normal, Slow = (0.002, 0.005, 0.01)
+
+    def __init__(self, unit_amount=130):
+        GPIO.setmode(GPIO.BCM)
+        self.__step_pins = [17,18,27,22]
+        # Set all pins as output
+        for pin in self.__step_pins:
+            #print "Setup pins"
+            GPIO.setup(pin,GPIO.OUT)
+            GPIO.output(pin, False)
+
+        self.__unit_amount = unit_amount
+
+        self.__cw_seq = (
+                (1, 0, 0, 0),
+                (0, 1, 0, 0),
+                (0, 0, 1, 0),
+                (0, 0, 0, 1),
+                )
+        self.__icw_seq = (
+                (0, 0, 0, 1),
+                (0, 0, 1, 0),
+                (0, 1, 0, 0),
+                (1, 0, 0, 0),
+                )
+
+
+    def run(self, quantity, direction=Clockwise, speed=Normal):
+        # Define some settings
+        if direction == StepMoter.Clockwise:
+            seq = self.__cw_seq
+        else:
+            seq = self.__icw_seq
+
+        counter = 0
+        while 1==1:
+            for pin in range(0, 4):
+                xpin = self.__step_pins[pin]
+                if seq[counter % len(seq)][pin]!=0:
+                    #print >> sys.stderr, 'put: %d -> %d' % (pin, xpin)
+                    GPIO.output(xpin, True)
+                else:
+                    GPIO.output(xpin, False)
+
+            # Wait before moving on
+            time.sleep(speed)
+            counter += 1
+            if counter / (self.__unit_amount * len(seq)) == quantity:
+                break
+
+if __name__ == '__main__':
+    s = StepMoter()
+    s.run( 
+        quantity = 6, 
+        direction = StepMoter.InterClockwise,
+        speed = StepMoter.Slow
+        )
--- step-motor/test.py
+++ step-motor/test.py
@@ -27,28 +27,28 @@ WaitTime = 0.02
 StepCount1 = 4
 Seq1 = []
 Seq1 = range(0, StepCount1)
-Seq1[0] = [1,0,0,0]
-Seq1[1] = [0,1,0,0]
-Seq1[2] = [0,0,1,0]
-Seq1[3] = [0,0,0,1]
+Seq1[0] = [0,0,0,1]
+Seq1[1] = [0,0,1,0]
+Seq1[2] = [0,1,0,0]
+Seq1[3] = [1,0,0,0]
 
 # Define advanced sequence
 # as shown in manufacturers datasheet
 StepCount2 = 8
 Seq2 = []
 Seq2 = range(0, StepCount2)
-Seq2[0] = [1,0,0,0]
-Seq2[1] = [1,1,0,0]
-Seq2[2] = [0,1,0,0]
-Seq2[3] = [0,1,1,0]
-Seq2[4] = [0,0,1,0]
-Seq2[5] = [0,0,1,1]
-Seq2[6] = [0,0,0,1]
-Seq2[7] = [1,0,0,1]
+Seq2[0] = [1,1,0,0]
+Seq2[1] = [0,1,0,0]
+Seq2[2] = [0,1,1,0]
+Seq2[3] = [0,0,1,0]
+Seq2[4] = [0,0,1,1]
+Seq2[5] = [0,0,0,1]
+Seq2[6] = [1,0,0,1]
+Seq2[7] = [1,0,0,0]
 
 # Choose a sequence to use
-Seq = Seq1
-StepCount = StepCount1
+Seq = Seq2
+StepCount = StepCount2
 
 # Start main loop
 while 1==1:
